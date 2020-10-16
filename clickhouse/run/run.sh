bf0c4e607836c4de265ec2d3d3d3dd5f77e62e8d
Alexey Dobrynin <gopooop@gmail.com>
2020-10-16 05:08:53 +0300

    change logs_api_integration project

--- clickhouse/run/run.sh
+++ clickhouse/run/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 if [[ ! -e "/usr/src/configs/config.json" ]]; then
-	git clone https://github.com/yndx-metrika/logs_api_integration.git /usr/src
+	git clone https://github.com/x5hell/logs_api_integration.git /usr/src
 	cd /usr/src
 	pip install requests
 	sed -i "s/<your_token>/${API_METRIKA_ACCESS_TOKEN}/" /usr/src/configs/config.json
