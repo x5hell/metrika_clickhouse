8d7348032d59dba2f64b2063c332de57331c6b55
Alexey Dobrynin <a.dobrynin@4slovo.ru>
2020-10-16 17:16:32 +0300

    fix clickhouse/run/run.sh

--- clickhouse/run/run.sh
+++ clickhouse/run/run.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
-if [[ ! -e "/usr/src/configs/config.json" ]]; then
+if [[ ! -e "/usr/src/configs/config.json.example" ]]; then
 	git clone https://github.com/x5hell/logs_api_integration.git /usr/src
 	cd /usr/src
 	pip install requests
+	cp /usr/src/configs/config.json.example /usr/src/configs/config.json
 	sed -i "s/<your_token>/${API_METRIKA_ACCESS_TOKEN}/" /usr/src/configs/config.json
 	sed -i "s/<your_counter_id>/${METRIKA_COUNTER_ID}/" /usr/src/configs/config.json
 fi
