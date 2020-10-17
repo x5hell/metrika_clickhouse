242f7a26ddda9d6607429c3ce9fcd9b3cc7e04b7
Alexey Dobrynin <gopooop@gmail.com>
2020-10-18 00:22:18 +0300

    fix run.sh

--- clickhouse/run/pull_data.sh
+++ clickhouse/run/pull_data.sh
@@ -3,4 +3,4 @@ set -ex
 sleep 30
 START_DATE=$(date  --date="14 days ago" +"%F")
 END_DATE=$(date  --date="1 days ago" +"%F")
-python2 metrica_logs_api.py -source visits -start_date $START_DATE -end_date $END_DATE
+python2 metrica_logs_api.py -source visits -start_date $START_DATE -end_date $END_DATE > /var/log/pull_data.log
