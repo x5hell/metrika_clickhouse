26ea2b0182e57bebec89459e8b17dbc9b08840b7
Alexey Dobrynin <gopooop@gmail.com>
2020-10-17 19:57:39 +0300

    fix clickhouse/run/run.sh

--- clickhouse/run/run.sh
+++ clickhouse/run/run.sh
@@ -9,8 +9,8 @@ if [[ ! -e "/usr/src/configs/config.json.example" ]]; then
 	cp /usr/src/configs/config.json.example /usr/src/configs/config.json
 	sed -i "s/<your_token>/${API_METRIKA_ACCESS_TOKEN}/" /usr/src/configs/config.json
 	sed -i "s/<your_counter_id>/${METRIKA_COUNTER_ID}/" /usr/src/configs/config.json
-        sed -i "s/<table_visits_all_field_list>/${VISITS_ALL_FIELDS}/" /usr/src/configs/config.json
-        sed -i "s/<table_hits_all_field_list>/${HITS_ALL_FIELDS}/" /usr/src/configs/config.json
+    sed -i "s/<table_visits_all_field_list>/${VISITS_ALL_FIELDS}/" /usr/src/configs/config.json
+    sed -i "s/<table_hits_all_field_list>/${HITS_ALL_FIELDS}/" /usr/src/configs/config.json
 fi
 
 /entrypoint.sh
